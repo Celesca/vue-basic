7c590f4503390ab45bd008107ca78d6be94989c8
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-06-22 16:37:22 +0700

    add: data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
-<script setup>
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        firstName: "Kong",
+        lastName:"Ruksiam",
+        age:15
+      }
+    }
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <section>
+  <h1>Hello World</h1>
+  </section>
 
 </template>
