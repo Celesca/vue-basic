a8508479d6076bb60e386b76c8f926cc05be2dfd
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-06-22 17:54:10 +0700

    add: array and object

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,21 @@
     <h2>{{ 500 + 200 }}</h2>
     <h2>อายุ : {{ age }}</h2>
     <h2>ที่อยู่ : <span v-html="address"></span></h2>
-    <a :href="social" target="_blank">Facebook</a>
+    <p> Social : <a :href="social" target="_blank">Facebook</a></p>
+    <p>งานอดิเรก</p>
+    <ul>
+      <li>{{ hobby[0] }}</li>
+      <li>{{ hobby[1] }}</li>
+      <li>{{ hobby[2] }}</li>
+    </ul>
+
+    <p>ข้อมูลทั่วไป</p>
+    <ul>
+      <li>เพศ : {{  general.gender }} </li>
+      <li>ส่วนสูง : {{  general.height }} เซนติเมตร</li>
+      <li>น้ำหนัก : {{  general.weight }} กิโลกรัม</li>
+      <li>สถานะ : {{  general.status }} </li>
+    </ul>
   </section>
 
 </template>
@@ -21,7 +35,14 @@
         address: "<i>กรุงเทพมหานคร</i>",
         picture: "https://cdn-icons-png.flaticon.com/128/16770/16770369.png",
         size: 50,
-        social: "https://www.facebook.com/kongruksiamtutorial"
+        social: "https://www.facebook.com/kongruksiamtutorial",
+        hobby: ["เล่นเกม", "อ่านหนังสือ", "เขียนโปรแกรม"],
+        general: {
+          gender: "ชาย",
+          height: 170,
+          weight: 60,
+          status: false
+        }
       }
     },
     methods: {
